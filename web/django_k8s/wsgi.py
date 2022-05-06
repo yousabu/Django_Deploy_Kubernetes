ee9ffcbf3e678c0523e2240e13f2ae527251c137
yousabu <shbanmhmdywsf8@gmail.com>
2022-05-06 15:25:54 +0200

    readme

--- web/django_k8s/wsgi.py
+++ web/django_k8s/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import pathlib
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_k8s.settings')
